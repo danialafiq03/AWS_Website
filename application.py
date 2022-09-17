6882cde6f82ee6c4eb69081ab346e449052ba8cd
Danial Afiq <danial.afiq.official@gmail.com>
2022-09-18 03:02:31 +0800

    added html & css content for index

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 application = Flask(__name__)
 app = application
@@ -6,6 +6,14 @@ app = application
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return "Contact Page"
+
+@app.route('/login')
+def login():
+    return "Login Page"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888)
