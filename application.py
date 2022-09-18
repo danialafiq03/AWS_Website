ad2958ad355e47e284bec656904ad729b6a53aa1
Danial Afiq <danial.afiq.official@gmail.com>
2022-09-18 22:34:16 +0800

    added Users List and guest sessions

--- application.py
+++ application.py
@@ -1,46 +1,73 @@
 from flask import Flask, render_template, url_for, session, request, g, redirect
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 import random
 
 application = Flask(__name__)
 app = application
 app.secret_key = 'secret123'
 socketio = SocketIO(app, cors_allowed_origins="*")
+usersList = []
+
+# @app.before_request
+# def before_request():
+#     if 'guest_id' not in session:
+#         random_num = random.randint(0, 10000)
+#         session['guest_id'] = random_num
+#         user = "Guest #" + str(session['guest_id'])
+#         usersList.append(user)
+#     print(usersList)
 
 @app.before_request
 def before_request():
-    # if 'user_id' in session:
-    #     try:
-    #         db = shelve.open('register.db', 'r')
-    #     except:
-    #         db = shelve.open('register.db', 'c')
-    #
-    #     user_dict = db['Users']
-    #     for key in user_dict:
-    #         if key == session['user_id']:
-    #             g.user = user_dict.get(key)
-    #     db.close()
-    # else:
-    random_num = random.randint(0, 10000)
-    session['guest_id'] = random_num
+    if 'guest_id' in session:
+        username = "Guest #" + str(session['guest_id'])
+        g.user = {"username": username}
 
 @socketio.on('message')
 def handle_message(message):
-    print("Received message: " + message)
-    if message != "User connected!":
+    if message[-10:] == 'connected!':
+        emit('conn_message', message, broadcast=True)
+    else:
         send(message, broadcast=True)
+
+@socketio.on('connects')
+def handle_connect(username):
+
+    if username not in usersList:
+        usersList.append(username)
+    print(usersList)
+    emit('users', usersList, broadcast=True)
+
+
+@socketio.on('disconnected')
+def handle_disconnect(username):
+    usersList.remove(username)
+    message = username + " has left the chat."
+    send(message, broadcast=True)
+    emit('users', usersList, broadcast=True)
+    print(usersList)
 
 @app.route('/')
 def index():
+    g.user = None
+    if not session.get("guest_id") or session.get("user_id"):
+        random_num = random.randint(0, 10000)
+        session['guest_id'] = random_num
+    if "guest_id" in session:
+        g.user = {"username": "Guest #" + str(session['guest_id'])}
+    elif "user_id" in session:
+        g.user = {"username": str(session["user_id"])}
     return render_template('index.html')
+
 
 @app.route('/contact')
 def contact():
-    return "Contact Page"
+    return str(g.user['username'])
+
 
 @app.route('/login')
 def login():
     return "Login Page"
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=80, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8888, allow_unsafe_werkzeug=True)
